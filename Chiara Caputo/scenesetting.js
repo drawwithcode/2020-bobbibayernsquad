eb8e2655ec79e05d04647bb681f63843b5ca8f6e
chcapu <chc.caputo@gmail.com>
2021-01-20 13:06:52 +0100

    cc files

---
+++ Chiara Caputo/scenesetting.js
@@ -0,0 +1,51 @@
+////////// SCENE SETTING //////////
+//Setting the scene: place the objects on the table and assign sounds on click
+
+class Item {
+  constructor(img) {
+    this.init();
+    this.img = img;
+  }
+
+  init() {
+    this.margin = 80;
+    this.x = floor(random(this.margin, width - this.margin));
+    this.y = floor(random(this.margin, height - this.margin));
+  }
+
+  show(imgw, imgh, ref) {
+    imageMode(CENTER);
+    //let scale1 = 0.25; //scaling based on the image saving settings
+    let scale = width / (1280 / ref * imgw); //scaling based on the window width and a reference proportion
+    this.w = imgw * scale;
+    this.h = this.w * imgh / imgw; //the height of the image always stays proportional to its width
+    image(this.img, this.x, this.y, this.w, this.h); //draw the image with the new set values
+    noLoop();
+  }
+
+  intersects(other) {
+  }
+
+  turn() {
+  }
+
+  clicked(snd) {
+    //let d = dist(mouseX, mouseY, this.x, this.y);
+    if (
+      mouseX > this.x - this.w/2 &&
+      mouseX < this.x + this.w/2 &&
+      mouseY > this.y - this.h/2 &&
+      mouseY < this.y + this.h/2
+      //!snd.isPlaying() //if a sound isn't already playing
+    ) {
+      snd.play();
+    }
+    // else {
+    //   sndTable.play();
+    // }
+   }
+
+  update() {
+  }
+
+}
---
+++ Chiara Caputo/sketch.js
@@ -0,0 +1,129 @@
+////////// GLOBAL VARIABLES FOR PAGE ELEMENTS //////////
+let cnv; //canvas
+let a; //instructions in the middle of the page
+let p; //other instructions
+
+////////// GLOBAL VARIABLES FOR THE ITEMS ON THE TABLE //////////
+//images
+let items = []; //the array of objects placed on the table, including:
+let imgBell;
+let imgCoins;
+let imgCupStanding;
+let imgGlasses;
+let imgKeys;
+let imgNotepad;
+let imgPapers;
+let imgPc;
+let imgPens;
+//sounds
+let sndBell;
+let sndCoins;
+let sndCupStanding;
+let sndGlasses;
+let sndKeys;
+let sndNotepad;
+let sndPapers;
+let sndPc;
+let sndPens;
+let sndTable = [];
+
+
+function preload(){
+  ////////// IMAGES PRELOAD //////////
+  imgBell = loadImage("./assets/images/bell.png");
+  imgCoins = loadImage("./assets/images/coins.png");
+  imgCupStanding = loadImage("./assets/images/cup-standing.png");
+  imgGlasses = loadImage("./assets/images/glasses.png");
+  imgKeys = loadImage("./assets/images/keys.png");
+  imgNotepad = loadImage("./assets/images/notepad.png");
+  imgPapers = loadImage("./assets/images/papers.png");
+  imgPc = loadImage("./assets/images/pc.png");
+  imgPens = loadImage("./assets/images/pens.png");
+
+  ////////// SOUNDS PRELOAD //////////
+  soundFormats("mp3", "wav");
+  sndBell = loadSound("./assets/sounds/bell.mp3");
+  sndCoins = loadSound("./assets/sounds/coins.mp3");
+  sndCupStanding = loadSound("./assets/sounds/cup-standing.mp3");
+  sndGlasses = loadSound("./assets/sounds/glasses.mp3");
+  sndKeys = loadSound("./assets/sounds/keys.mp3");
+  sndNotepad = loadSound("./assets/sounds/notepad.mp3");
+  sndPapers = loadSound("./assets/sounds/papers.mp3");
+  sndPc = loadSound("./assets/sounds/pc.mp3");
+  sndPens = loadSound("./assets/sounds/pens.mp3");
+  for (let i = 0; i < 2; i++) {
+    sndTable[i] = loadSound("./assets/sounds/table" + i + ".mp3");
+  }
+}
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function setup() {
+  ////////// CANVAS SETTINGS //////////
+  cnv = createCanvas(windowWidth,windowHeight);
+  centerCanvas();
+  cnv.style('background-color', 'white');
+
+  ////////// SCENE SETTING //////////
+  items[0] = new Item(imgBell);
+  items[1] = new Item(imgCoins);
+  items[2] = new Item(imgCupStanding);
+  items[3] = new Item(imgGlasses);
+  items[4] = new Item(imgKeys);
+  items[5] = new Item(imgNotepad);
+  items[6] = new Item(imgPapers);
+  items[7] = new Item(imgPc);
+  items[8] = new Item(imgPens);
+
+  ////////// PAGE ELEMENTS //////////
+  //click to...
+  p = createP('Click to feel the table');
+  p.style("font-family", "Inconsolata");
+  p.style("font-size", "14px");
+  p.style("text-transform", "uppercase");
+  p.position(50,100);
+
+  //instructions in the middle of the page
+  a = createDiv("<p>There is a table in front of you...</p><p>Try to find the bell</p>");
+  a.style("font-family", "Work Sans");
+  a.style("font-weight", "400");
+  a.style("font-size", "18px");
+  a.style("line-height", "14px");
+  a.style("text-align", "center");
+  a.position(300,100);
+
+}
+
+function mousePressed() {
+  items[0].clicked(sndBell);
+  items[1].clicked(sndCoins);
+  items[2].clicked(sndCupStanding);
+  items[3].clicked(sndGlasses);
+  items[4].clicked(sndKeys);
+  items[5].clicked(sndNotepad);
+  items[6].clicked(sndPapers);
+  items[7].clicked(sndPc);
+  items[8].clicked(sndPens);
+}
+
+function draw() {
+  ////////// SCENE SETTING //////////
+  items[0].show(imgBell.width, imgBell.height, 103);
+  items[1].show(imgCoins.width, imgCoins.height, 50);
+  items[2].show(imgCupStanding.width, imgCupStanding.height, 155);
+  items[3].show(imgGlasses.width, imgGlasses.height, 214);
+  items[4].show(imgKeys.width, imgKeys.height, 63);
+  items[5].show(imgNotepad.width, imgNotepad.height, 123);
+  items[6].show(imgPapers.width, imgPapers.height, 253);
+  items[7].show(imgPc.width, imgPc.height, 429);
+  items[8].show(imgPens.width, imgPens.height, 223);
+}
+
+
+function windowResized() {
+  centerCanvas();
+}
