d5ca2efd2802ae244a07222e29ec5c362e974eb2
MaurizioCerisola <66005558+MaurizioCerisola@users.noreply.github.com>
2021-01-21 13:43:03 +0100

    Solvede overlapping issues

--- Chiara Caputo/scenesetting.js
+++ Chiara Caputo/scenesetting.js
@@ -2,24 +2,32 @@
 //Setting the scene: place the objects on the table and assign sounds on click
 
 class Item {
-  constructor(img) {
-    this.init();
+  constructor(img, snd, x, y, ref) {
+    //Item position
+    this.x = x;
+    this.x_normalized = map(this.x,0,windowWidth,0,1);
+    this.y = y;
+    this.y_normalized = map(this.y,0,windowHeight,0,1);
+
+    //Item dimensions
+    this.ref = ref;
+    let windowDiagonal = pow(pow(windowHeight,2)+pow(windowWidth,2),0.5);
+    let scale = windowDiagonal*0.8 / (1280 / this.ref * img.width);
+    this.w = img.width*scale;
+    this.h = this.w * img.height / img.width; //the height of the image always stays proportional to its width
+
+    //Items image
     this.img = img;
+
+    //Items sound
+    this.snd=snd ;
   }
 
-  init() {
-    this.margin = 80;
-    this.x = floor(random(this.margin, width - this.margin));
-    this.y = floor(random(this.margin, height - this.margin));
-  }
-
-  show(imgw, imgh, ref) {
+  show() {
+    push();
     imageMode(CENTER);
-    let scale = width / (1280 / ref * imgw); //scaling based on the window width and a reference proportion
-    this.w = imgw * scale;
-    this.h = this.w * imgh / imgw; //the height of the image always stays proportional to its width
     image(this.img, this.x, this.y, this.w, this.h); //draw the image with the new set values
-    //noLoop();
+    pop();
   }
 
   clicked() {
@@ -35,8 +43,17 @@ class Item {
     }
   }
 
-  play(snd) {
-    snd.play();
+  scale() {
+    this.x=map(this.x_normalized,0,1,0,windowWidth);
+    this.y=map(this.y_normalized,0,1,0,windowHeight);
+    let windowDiagonal = pow(pow(windowHeight,2)+pow(windowWidth,2),0.5);
+    let scale = windowDiagonal*0.8 / (1280 / this.ref * this.img.width);
+    this.w = this.img.width*scale;
+    this.h = this.w * this.img.height / this.img.width; //the height of the image always stays proportional to its width
+  }
+
+  play() {
+    this.snd.play();
   }
 
 }
--- Chiara Caputo/sketch.js
+++ Chiara Caputo/sketch.js
@@ -1,6 +1,5 @@
 ////////// GLOBAL VARIABLES FOR PAGE ELEMENTS & OTHER //////////
 let cnv; //canvas
-let shown = false;
 let success = false; //the bell has been found: end of the experience
 
 let theX = 1;
@@ -8,52 +7,49 @@ let theY = 1;
 let easing = 0.1;
 
 ////////// GLOBAL VARIABLES FOR THE ITEMS ON THE TABLE //////////
-//images
-let items = []; //the array of objects placed on the table, including:
-let imgBell;
-let imgCoins;
-let imgCupStanding;
-let imgGlasses;
-let imgKeys;
-let imgNotepad;
-let imgPapers;
-let imgPc;
-let imgPens;
-//sounds
-let sndBell;
-let sndCoins;
-let sndCupStanding;
-let sndGlasses;
-let sndKeys;
-let sndNotepad;
-let sndPapers;
-let sndPc;
-let sndPens;
+let items = [];
+let itemsPosition = [];
+let itemsDimensions = [];
+let images = [];
+let sounds = [];
 let sndTable = [];
+let refs = [];
 
 
 function preload() {
   ////////// IMAGES PRELOAD //////////
-  imgBell = loadImage("./assets/images/bell.png");
-  imgCoins = loadImage("./assets/images/coins.png");
-  imgCupStanding = loadImage("./assets/images/cup-standing.png");
-  imgGlasses = loadImage("./assets/images/glasses.png");
-  imgKeys = loadImage("./assets/images/keys.png");
-  imgNotepad = loadImage("./assets/images/notepad.png");
-  imgPapers = loadImage("./assets/images/papers.png");
-  imgPc = loadImage("./assets/images/pc.png");
-  imgPens = loadImage("./assets/images/pens.png");
+  images.push(loadImage("./assets/images/papers.png"));
+  refs.push(253);
+  images.push(loadImage("./assets/images/pc.png"));
+  refs.push(429);
+  images.push(loadImage("./assets/images/pens.png"));
+  refs.push(223);
+  images.push(loadImage("./assets/images/glasses.png"));
+  refs.push(214);
+  images.push(loadImage("./assets/images/cup-standing.png"));
+  refs.push(155);
+  images.push(loadImage("./assets/images/notepad.png"));
+  refs.push(123);
+  images.push(loadImage("./assets/images/bell.png"));
+  refs.push(103);
+  images.push(loadImage("./assets/images/keys.png"));
+  refs.push(63);
+  images.push(loadImage("./assets/images/coins.png"));
+  refs.push(50);
+
   ////////// SOUNDS PRELOAD //////////
   soundFormats("mp3", "wav");
-  sndBell = loadSound("./assets/sounds/bell.mp3");
-  sndCoins = loadSound("./assets/sounds/coins.mp3");
-  sndCupStanding = loadSound("./assets/sounds/cup-standing.mp3");
-  sndGlasses = loadSound("./assets/sounds/glasses.mp3");
-  sndKeys = loadSound("./assets/sounds/keys.mp3");
-  sndNotepad = loadSound("./assets/sounds/notepad.mp3");
-  sndPapers = loadSound("./assets/sounds/papers.mp3");
-  sndPc = loadSound("./assets/sounds/pc.mp3");
-  sndPens = loadSound("./assets/sounds/pens.mp3");
+  sounds.push(loadSound("./assets/sounds/papers.mp3")); //0
+  sounds.push(loadSound("./assets/sounds/pc.mp3")); //1
+  sounds.push(loadSound("./assets/sounds/pens.mp3")); //2
+  sounds.push(loadSound("./assets/sounds/glasses.mp3")); //3
+  sounds.push(loadSound("./assets/sounds/cup-standing.mp3")); //4
+  sounds.push(loadSound("./assets/sounds/notepad.mp3")); //5
+  sounds.push(loadSound("./assets/sounds/bell.mp3")); //6
+  sounds.push(loadSound("./assets/sounds/keys.mp3"));
+  sounds.push(loadSound("./assets/sounds/coins.mp3"));
+
+
   for (let i = 0; i < 2; i++) {
     sndTable[i] = loadSound("./assets/sounds/table" + i + ".mp3");
   }
@@ -73,15 +69,13 @@ function setup() {
   //cnv.style('background-color', 'white');
 
   ////////// SCENE SETTING //////////
-  items[0] = new Item(imgBell);
-  items[1] = new Item(imgCoins);
-  items[2] = new Item(imgCupStanding);
-  items[3] = new Item(imgGlasses);
-  items[4] = new Item(imgKeys);
-  items[5] = new Item(imgNotepad);
-  items[6] = new Item(imgPapers);
-  items[7] = new Item(imgPc);
-  items[8] = new Item(imgPens);
+  let margin = 80;
+  for (let i=0; i<images.length; i++) {
+    let xy = getXY(margin, i);
+    items.push(new Item(images[i],sounds[i],xy[0],xy[1],refs[i]));
+    itemsPosition.push([items[i].x, items[i].y]);
+    itemsDimensions.push([items[i].w, items[i].h]);
+  }
 
   ////////// PAGE ELEMENTS //////////
   //...
@@ -92,33 +86,21 @@ function mousePressed() {
   ////////// CLICK EVENTS //////////
   //whenever an object is clicked, its specific sound is played
   //items that are shown on top of others are played first, as they are the ones to be touched by the user in an overlapping scenario
-  if (items[0].clicked()) {
-    items[0].play(sndBell);
-
-    //the page changes since the activity has finished
-    success = true;
-    //document.getElementById("t").style.color = "black";
-    let a = select('#t');
-    a.style('color', 'black');
-
-  } else if (items[8].clicked()) {
-    items[8].play(sndPens);
-  } else if (items[2].clicked()) {
-    items[2].play(sndCupStanding);
-  } else if (items[4].clicked()) {
-    items[4].play(sndKeys);
-  } else if (items[1].clicked()) {
-    items[1].play(sndCoins);
-  } else if (items[3].clicked()) {
-    items[3].play(sndGlasses);
-  } else if (items[5].clicked()) {
-    items[5].play(sndNotepad);
-  } else if (items[6].clicked()) {
-    items[6].play(sndPapers);
-  } else if (items[7].clicked()) {
-    items[7].play(sndPc);
-  } else {
-  //whenever the user clicks elsewhere, the sound of the table being touched is played
+  let soundPlayed = false;
+  for (let i=0;i<items.length;i++) {
+    if (items[i].clicked()) {
+      items[i].play();
+      soundPlayed=true;
+      if (i==6) {
+        //the page changes since the activity has finished
+        success = true;
+        //document.getElementById("t").style.color = "black";
+        let a = select('#t');
+        a.style('color', 'black');
+      }
+    }
+  }
+  if (!soundPlayed) {
     let anySndTable = random(sndTable);
     anySndTable.play();
   }
@@ -129,27 +111,15 @@ function draw() {
   background("white");
 
   //first drawing the objects that are more likely to be overlapped by others in a real desk setting, then the others
-  items[7].show(imgPc.width, imgPc.height, 429);
-  items[6].show(imgPapers.width, imgPapers.height, 253);
-  items[5].show(imgNotepad.width, imgNotepad.height, 123);
-  items[3].show(imgGlasses.width, imgGlasses.height, 214);
-  items[1].show(imgCoins.width, imgCoins.height, 50);
-  items[4].show(imgKeys.width, imgKeys.height, 63);
-  items[2].show(imgCupStanding.width, imgCupStanding.height, 155);
-  items[8].show(imgPens.width, imgPens.height, 223);
-  items[0].show(imgBell.width, imgBell.height, 103);
-
-  //avoiding for the objects to overlap on one another
-  isOverlap();
-  if (!shown) {
-    shown = true;
-    solveOverlap();
+  for (let i=0; i<items.length; i++) {
+    items[i].scale();
+    items[i].show();
   }
 
   //covering the canvas so that the user does not see the items on the table
   if (!success) {
-    fill("black");
-    rect(0, 0, windowWidth*2, windowHeight*2);
+    //fill("black");
+    //rect(0, 0, windowWidth*2, windowHeight*2);
   }
 
   //ellipse following the mouse
@@ -168,49 +138,42 @@ function draw() {
 
 }
 
-function solveOverlap() {
-  let i = 0;
-  while (isOverlap() && i < 200) {
-    i++;
-    console.log("!");
-  }
-}
-
-function isOverlap() {
-  let result = false;
-  for (var i = 0; i < items.length; i++) {
-    let diagi = dist(0, 0, items[i].w/2, items[i].h/2);
-    for (var j = 0; j < items.length; j++) {
-      let diagj = dist(0, 0, items[j].w/2, items[j].h/2);
-      if (i !=j && dist(items[i].x, items[i].y, items[j].x, items[j].y) < (diagi + diagj)/1.1) {
-        let dx = 0;
-        let dy = 0;
-        let speed = 10;
-        if (items[j].x > items[i].x) {
-          dx += speed;
-        } else {
-          dx -= speed;
-        }
-        if (items[j].y > items[i].y) {
-          dy += speed;
-        } else {
-          dy -= speed;
-        }
-        if (dx + items[j].x < 80 || dx + items[j].x > windowWidth - 80) {
-          dx = 0;
-        }
-        if (dy + items[j].y < 80 || dy + items[j].y > windowHeight - 80) {
-          dy = 0;
-        }
-        if (dx !=0 || dy !=0) {
-          items[j].x += dx;
-          items[j].y += dy;
-          result = true;
-        }
-      }
+function checkOverlaps (x,y) {
+  for (let i=0; i<itemsPosition.length; i++) {
+    if (x > itemsPosition[i][0] - itemsDimensions[i][0]/2 &&
+        x < itemsPosition[i][0] + itemsDimensions[i][0]/2 &&
+        y > itemsPosition[i][1] - itemsDimensions[i][1]/2 &&
+        y < itemsPosition[i][1] + itemsDimensions[i][1]/2) {
+      return true;
     }
   }
-  return result;
+  return false;
+}
+
+function getXY (margin, i) {
+  let x;
+  let y;
+  let windowDiagonal = pow(pow(windowHeight,2)+pow(windowWidth,2),0.5);
+  let scale = windowDiagonal*0.8 / (1280 / refs[i] * images[i].width);
+  let w = images[i].width*scale;
+  let h = w * images[i].height / images[i].width; //the height of the image always stays proportional to its width
+  let overlapped =true;
+  let j=0;
+  while (overlapped) {
+    x = floor(random(margin, windowWidth - margin));
+    y = floor(random(margin, windowHeight - margin));
+    if (!(checkOverlaps(x,y) ||
+        checkOverlaps(x-w/2,y-h/2) || checkOverlaps(x-w/2,y+h/2) ||
+        checkOverlaps(x+w/2,y-h/2) || checkOverlaps(x+w/2,y+h/2))) {
+          overlapped = false;
+    }
+    j++;
+    console.log(j);
+  }
+  return [x,y];
+
+
+
 }
 
 function windowResized() {
